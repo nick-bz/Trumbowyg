5e9a6d4202f791f12f5d2123acb056c69186a9c2
nick-bz <108975428+nick-bz@users.noreply.github.com>
2022-07-10 12:48:09 +0300

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,12 @@
 // jshint node:true
 'use strict';
 
+const sass = require('gulp-sass')(require('sass'));
 var gulp = require('gulp'),
     del = require('del'),
     vinylPaths = require('vinyl-paths'),
     $ = require('gulp-load-plugins')();
+const concat = require('gulp-concat');
 
 var paths = {
     scripts: ['src/trumbowyg.js'],
@@ -38,29 +40,17 @@ var bannerLight = ['/** <%= pkg.title %> v<%= pkg.version %> - <%= pkg.descripti
     '\n'].join('');
 
 
-gulp.task('clean', function () {
+gulp.task('clean', async () => {
     return gulp.src('dist/*')
         .pipe(vinylPaths(del));
 });
 
-gulp.task('test', ['test-scripts', 'test-langs', 'test-plugins']);
-gulp.task('test-scripts', function () {
+gulp.task('test-scripts', async () => {
     return gulp.src(paths.scripts)
         .pipe($.jshint())
         .pipe($.jshint.reporter('jshint-stylish'));
 });
-gulp.task('test-langs', function () {
-    return gulp.src(paths.langs)
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'));
-});
-gulp.task('test-plugins', function () {
-    return gulp.src(paths.plugins)
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('scripts', ['test-scripts'], function () {
+gulp.task('scripts', gulp.series('test-scripts', async () => {
     return gulp.src(paths.scripts)
         .pipe($.header(banner, {pkg: pkg, description: 'Trumbowyg core file'}))
         .pipe($.newer('dist/trumbowyg.js'))
@@ -72,26 +62,39 @@ gulp.task('scripts', ['test-scripts'], function () {
         .pipe($.header(bannerLight, {pkg: pkg}))
         .pipe(gulp.dest('dist/'))
         .pipe($.size({title: 'trumbowyg.min.js'}));
-});
+}));
 
-gulp.task('langs', ['test-langs'], function () {
+gulp.task('test-langs', async () => {
     return gulp.src(paths.langs)
-        .pipe($.rename({suffix: '.min'}))
-        .pipe($.uglify({
-            preserveComments: 'all'
-        }))
-        .pipe(gulp.dest('dist/langs/'));
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'));
+});
+gulp.task('test-plugins', async () => {
+    return gulp.src(paths.plugins)
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('plugins', ['test-plugins'], function () {
+gulp.task('plugins', gulp.series('test-plugins', async () => {
     return gulp.src(paths.plugins)
         .pipe(gulp.dest('dist/plugins/'))
         .pipe($.rename({suffix: '.min'}))
         .pipe($.uglify())
         .pipe(gulp.dest('dist/plugins/'));
-});
+}));
 
+gulp.task('langs', gulp.series('test-langs', async () => {
+    return gulp.src(paths.langs)
+        .pipe($.rename({suffix: '.min'}))
+        .pipe($.uglify({
+            //preserveComments: 'all'
+        }))
+        .pipe(gulp.dest('dist/langs/'));
+}));
 
+gulp.task('test', gulp.series('test-scripts', 'test-langs', 'test-plugins', async (done) => {
+    //done();
+}));
 
 gulp.task('icons', function () {
     return gulp.src(paths.icons)
@@ -101,13 +104,13 @@ gulp.task('icons', function () {
         .pipe(gulp.dest('dist/ui/'));
 });
 
-
-
-gulp.task('styles', function () {
-    return gulp.src(paths.mainStyle)
-        .pipe($.sass({
-            sass: paths.styles.sass
-        }))
+gulp.task('styles', async () => {
+    return gulp.src(paths.styles.sass)
+        .pipe(sass.sync().on('error', sass.logError)) // scss to css
+        // .pipe(sass({
+        //     sass: paths.styles.sass
+        // }))
+        .pipe(concat(paths.mainStyle))
         .pipe($.autoprefixer(['last 1 version', '> 1%', 'ff >= 20', 'ie >= 9', 'opera >= 12', 'Android >= 2.2'], {cascade: true}))
         .pipe($.header(banner, {pkg: pkg, description: 'Default stylesheet for Trumbowyg editor'}))
         .pipe(gulp.dest('dist/ui/'))
@@ -119,15 +122,15 @@ gulp.task('styles', function () {
         .pipe($.size({title: 'trumbowyg.min.css'}));
 });
 
-
-gulp.task('sass-dist', ['styles'], function () {
+gulp.task('sass-dist', gulp.series('styles', async () => {
     return gulp.src('src/ui/sass/**/*.scss')
         .pipe($.header(banner, {pkg: pkg, description: 'Default stylesheet for Trumbowyg editor'}))
         .pipe(gulp.dest('dist/ui/sass'));
-});
+}));
 
 
-gulp.task('watch', function () {
+gulp.task('watch', async (done) => {
+    //done();
     gulp.watch(paths.icons, ['icons']);
     gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.langs, ['langs']);
@@ -141,6 +144,10 @@ gulp.task('watch', function () {
     $.livereload.listen();
 });
 
-gulp.task('build', ['scripts', 'langs', 'plugins', 'sass-dist', 'icons']);
+gulp.task('build', gulp.series('scripts', 'langs', 'plugins', 'sass-dist', 'icons', async (done) => {
+    //done();
+}));
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', gulp.series('build', 'watch', async (done) => {
+    //done();
+}));
